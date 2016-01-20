d7f1f5cc42963def41aec297cc985d85cec652f9
Mark Greenwald <markerdink@gmail.com>
2016-01-20 06:38:08 -0800

    Reusable UI Found
    The secret of the reusable UI has been revealed. I feel dumb for not
    spotting it sooner. To be fair I don't see any documentation for any of
    this anywhere. DOH!

--- Source/DinkPDN.Effects.Extended/ConfigurableEffect.cs
+++ Source/DinkPDN.Effects.Extended/ConfigurableEffect.cs
@@ -8,6 +8,8 @@ using System.Drawing;
 using System.Reflection;
 using System.Windows.Forms;
 using DinkPDN.UI.Controls;
+using PaintDotNet.PropertySystem;
+using System.Linq;
 
 namespace DinkPDN.Effects.Extended
 {
@@ -39,23 +41,149 @@ namespace DinkPDN.Effects.Extended
                 .ToDictionary(p => p.prop, p => p.attr);
         }
 
-        protected class ConfigurableAttribute : Attribute
+        protected abstract class ConfigurableAttribute : Attribute
         {
             public string Name { get; set; }
             public string Description { get; set; }
-            public PropertyControlDescriptor.ExperienceTypes Experience { get; set; }
-            public object Default { get; set; }
-            public IComparable Min { get; set; }
-            public IComparable Max { get; set; }
-            public IComparable MicroStep { get; set; }
-            public IComparable SmallStep { get; set; }
-            public IComparable LargeStep { get; set; }
-            public object Precision { get; set; }
-            public bool Multiline { get; set; }
-            public string DelimitedValues { get; set; }
-            public virtual Control Create(PropertyInfo property)
+            public abstract Control Create(PropertyInfo property);
+            protected Control CreateHost<TType>(TType property) where TType : Property
             {
-                var control = 
+                var descriptor = PropertyControlDescriptor.All.First(d => d.PropertyType.IsAssignableFrom(typeof(TType)));
+                return new PropertyControlHost(descriptor, property);
+            }
+        }
+
+        protected class ConfigurableImageAttribute : ConfigurableAttribute
+        {
+            public override Control Create(PropertyInfo property) {
+                return CreateHost(
+                    new ImageProperty(Name)
+                );
+            }
+        }
+
+        protected class ConfigurableVectorAttribute : ConfigurableAttribute
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public object MaxX { get; set; }
+            public object MaxY { get; set; }
+            public object MinX { get; set; }
+            public object MinY { get; set; }
+
+            public override Control Create(PropertyInfo property)
+            {
+                return CreateHost(
+                    new DoubleVectorProperty(
+                        property.Name,
+                        new Pair<double, double>(X, Y),
+                        new Pair<double, double>(MinX.GetValue(double.MinValue), MinY.GetValue(double.MinValue)),
+                        new Pair<double, double>(MaxX.GetValue(double.MaxValue), MaxY.GetValue(double.MaxValue)))
+                );
+            }
+        }
+
+        protected class ConfigurableVector3Attribute : ConfigurableVectorAttribute
+        {
+            public double Z { get; set; }
+            public object MaxZ { get; set; }
+            public object MinZ { get; set; }
+
+            public override Control Create(PropertyInfo property)
+            {
+                return CreateHost(
+                    new DoubleVector3Property(
+                        property.Name,
+                        new Tuple<double, double, double>(X, Y, Z),
+                        new Tuple<double, double, double>(MinX.GetValue(double.MinValue), MinY.GetValue(double.MinValue), MinZ.GetValue(double.MinValue)),
+                        new Tuple<double, double, double>(MaxX.GetValue(double.MaxValue), MaxY.GetValue(double.MaxValue), MaxZ.GetValue(double.MaxValue)))
+                );
+            }
+        }
+
+        protected abstract class DefaultConfigurableAttribute : ConfigurableAttribute
+        {
+            public object Default { get; set; }
+        }
+
+        protected class ConfigurableBoolAttribute : DefaultConfigurableAttribute
+        {
+            public override Control Create(PropertyInfo property)
+            {
+                return CreateHost(
+                    new BooleanProperty(
+                        property.Name,
+                        Default.GetValue(false))
+                );
+            }
+        }
+
+        protected class ConfigurableStringAttribute : DefaultConfigurableAttribute
+        {
+            public object MaxLength { get; set; }
+            public bool Multiline { get; set; }
+            public override Control Create(PropertyInfo property)
+            {
+                return CreateHost(
+                    new StringProperty(
+                    property.Name,
+                    Default.GetValue(""),
+                    MaxLength.GetValue(int.MaxValue))
+                );
+            }
+        }
+
+        protected class ConfigurableSelectionAttribute : DefaultConfigurableAttribute
+        {
+            public string DelimitedValues { get; set; }
+            public override Control Create(PropertyInfo property)
+            {
+                return CreateHost(
+                    new StaticListChoiceProperty(
+                        property.Name,
+                        (DelimitedValues ?? "").ToString()
+                        .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Cast<object>().ToArray(),
+                        Default.GetValue(0))
+                );
+            }
+        }
+
+        protected abstract class NumericConfigurableAttribute : DefaultConfigurableAttribute
+        {
+            public object Min { get; set; }
+            public object Max { get; set; }
+            public object SmallStep { get; set; }
+            public object LargeStep { get; set; }
+            public object MicroStep { get; set; }
+        }
+
+        protected class ConfigurableIntAttribute : NumericConfigurableAttribute
+        {
+            public override Control Create(PropertyInfo property)
+            {
+                return CreateHost(
+                    new Int32Property(
+                        property.Name,
+                        Default.GetValue(0),
+                        Min.GetValue(0),
+                        Max.GetValue(255))
+                );
+            }
+        }
+
+        protected class ConfigurableDoubleAttribute : NumericConfigurableAttribute
+        {
+            public object Precision { get; set; }
+            public override Control Create(PropertyInfo property)
+            {
+                return CreateHost(
+                    new DoubleProperty(
+                        property.Name,
+                        Default.GetValue(0.5d),
+                        Min.GetValue(0d),
+                        Max.GetValue(1d))
+                );
             }
         }
     }
--- Source/Tile/Plugin.cs
+++ Source/Tile/Plugin.cs
@@ -1,28 +1,38 @@
-﻿using DinkPDN.Effects.Simple;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PaintDotNet;
-using DinkPDN.UI.Controls;
 using PaintDotNet.PropertySystem;
 using PaintDotNet.IndirectUI;
+using PaintDotNet.Effects;
+using DinkPDN.Common;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Reflection;
 
 namespace Tile
 {
-    public class Plugin : ConfigurablePixelEffect
+    [PluginSupportInfo(typeof(AssemblyPluginSupportInfo))]
+    public class Plugin : Effect
     {
-        public Plugin() : base("Test") { }
-
-        protected override ColorBgra RenderPixel(int x, int y, ColorBgra initial, Surface source)
-        {
-            var stuff = PropertyControlDescriptor.All;
-            var prop = new DoubleProperty("Test", 0d, 0d, 1d);
+        public Plugin() : base("Test", new AssemblyPluginSupportInfo().Icon, "Test", EffectFlags.Configurable) {
+            var prop = new DoubleProperty("Test", 0d, 0d, 10d);
             var info = PropertyControlInfo.CreateFor(prop);
-            var ctrl = (PropertyControl)Activator.CreateInstance(stuff.First().ControlType, info);
-            ctrl.Property.
-            return initial;
+            info.SetPropertyControlValue("Test", ControlInfoPropertyNames.DisplayName, "Name");
+            info.SetPropertyControlValue("Test", ControlInfoPropertyNames.Description, "Desc");
+
+            var win = new Form();
+            var control = (Control)info.CreateConcreteControl(win);
+            control.Dock = DockStyle.Fill;
+            win.Controls.Add((Control)control);
+            win.ShowDialog();
+        }
+
+        public override void Render(EffectConfigToken parameters, RenderArgs dstArgs, RenderArgs srcArgs, Rectangle[] rois, int startIndex, int length)
+        {
+
         }
     }
 }
